74a782a76d6f86147a9972ec40fa6869ab962af7
bhatarchanas <bhatarchanas@yahoo.com>
2015-07-14 16:50:14 -0400

    Added - input file having all the assemblies to be read

--- get_assembly_stats.rb
+++ get_assembly_stats.rb
@@ -1,16 +1,23 @@
 require 'json'
 
-file_names = ARGV
+file_names = []
+File.open("Assembly_files_to_read.txt", "r") do |f|
+  f.each_line do |line|
+    file_names = line.split(" ")
+  end
+end
+
 #puts file_names                                                                                                                                              
 #puts file_names.class                                                                                                                                        
 
 output_file = open("Output_assembly_results.txt", "w")
 output_file.puts "ID\tPolished_contigs\tMax_contig_length\tN50_contig_length\tSum_of_contig_lengths\n"
 
-for each in 0..file_names.size-1
-  file = File.read("/data/pacbio/smrtanalysis_userdata/jobs/017/0170"+file_names[each].to_s+"/results/polished_report.json")
-  data_hash = JSON.parse(file)
-  output_file.puts "0170"+file_names[each].to_s+"\t"+data_hash["attributes"][0]["value"].to_s+"\t"+data_hash["attributes"][1]["value"].to_s+"\t"+data_hash["a\
-ttributes"][2]["value"].to_s+"\t"+data_hash["attributes"][3]["value"].to_s+"\n"
-end
+prefix = "017"
 
+for each in 0..file_names.size-1
+  file = File.read("/data/pacbio/smrtanalysis_userdata/jobs/#{prefix}/#{prefix}0"+file_names[each].to_s+"/results/polished_report.json")
+  data_hash = JSON.parse(file)
+  output_file.puts "#{prefix}0"+file_names[each].to_s+"\t"+data_hash["attributes"][0]["value"].to_s+"\t"+data_hash["attributes"][1]["value"].to_s+"\t"+data_h\
+ash["attributes"][2]["value"].to_s+"\t"+data_hash["attributes"][3]["value"].to_s+"\n"
+end
